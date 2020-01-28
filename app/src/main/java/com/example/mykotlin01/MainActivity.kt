bbe938b8381de1b755542d1ed3f20d2788dff7c1
wim de boeck <wdboeck@sckcen.be>
2020-01-28 17:03:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mykotlin01/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mykotlin01
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var t1:TextView = findViewById(R.id.textView) as TextView
+        var t2:TextView = findViewById(R.id.textView2) as TextView
+        var t3:TextView = findViewById(R.id.textView3) as TextView
+
+        t1.setText("Android version = " + Build.VERSION.RELEASE)
+        t2.setText("API niveau = " + Build.VERSION.SDK_INT)
+        t3.setText("Apparaat = " + Build.MANUFACTURER)
+    }
+}
